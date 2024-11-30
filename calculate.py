701d9336c1fe5dc2df06e073c16ee71379723670
chins58 <chintanshah58@gmail.com>
2024-11-30 10:52:34 +0530

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -32,13 +32,13 @@ end_year = begin_year + year
 for y in range(begin_year, end_year):
     if (judge_leap_year(y)):
         day = day + 366
-    else:
-        day = day + 365
+    else: 
+        day = day + 365 
 
 leap_year = judge_leap_year(localtime.tm_year)
 for m in range(1, localtime.tm_mon):
     day = day + month_days(m, leap_year)
 
-day = day + localtime.tm_mday
+day = day + localtime.tm_mday 
 print("\n\t%s's age is %d years or " % (name, year), end="")
-print("%d months or %d days" % (month, day))
+print("%d months or %d days" % (month, day)) 
